cdb704b553cde67502596022f839b222ea0a8df5
lyra <mk.lyra.j@gmail.com>
2025-01-09 17:12:34 +0900

    update: 컨텐츠 정렬 css 수정

--- scripts/api.js
+++ scripts/api.js
@@ -1,21 +1,22 @@
 const API_KEY = config.apikey;
+const API_TOKEN = config.apitoken;
 
 function getMovies() {
   const url =
-    "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1";
+    "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1";
 
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: `Bearer ${API_KEY}`,
+      Authorization: `Bearer ${API_TOKEN}`,
     },
   };
 
   fetch(url, options)
     .then((res) => res.json())
     .then((data) => {
-      const dataIndex = data["results"];
+      const dataIndex = data["results"]; // 받아온 자료들 할당
       const movieList = document.querySelector("#movie-list");
 
       movieList.innerHTML = "";
@@ -23,19 +24,19 @@ function getMovies() {
       dataIndex.forEach((e) => {
         const movie_id = e["id"];
         const movie_poster =
-          "https://image.tmdb.org/t/p/w200/" + e["poster_path"];
+          "https://image.tmdb.org/t/p/w500/" + e["poster_path"];
         const movie_title = e["title"];
         const movie_average = e["vote_average"];
 
-        let temp_html = `<img src="${movie_poster}" alt="${movie_title}" />
+        let temp_html = `<img class="thumbnail" src="${movie_poster}" alt="${movie_title}" />
       <div>
         <h3>${movie_title}</h3>
         <p>평점 : ${movie_average}</p>
       </div>`;
-        const movieDiv = document.createElement("div");
+        const movieDiv = document.createElement("div"); // 컨텐츠가 들어갈 div 박스 생성 후 클래스명 주기
         movieDiv.className += "movieCards";
-        movieDiv.innerHTML = temp_html;
-        movieList.appendChild(movieDiv);
+        movieDiv.innerHTML = temp_html; //  받아온 자료로 html에 원하는 값만 붙여넣기
+        movieList.appendChild(movieDiv); // movieList의 하위 노드로 붙여 넣기
       });
     })
     .catch((err) => console.error(err));
